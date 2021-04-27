7a021bbfc4d00e95ef347c30cee52fddfefdc7a4
Dylan Davies <dylandaviddavies@gmail.com>
2021-04-27 17:19:55 -0400

    Add d3

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,13 @@
 <template>
-  <a-scene>
-    <a-box
-        color="#0095DD"
-        position="0 1 0"
-        rotation="20 40 0">
-    </a-box>
-    <a-camera
+  <a-scene v-pre>
+    <a-plane v-pre rotation="-90 0 0" width="30" height="30"></a-plane>
+
+    <a-light v-pre type="ambient" color="#445451"></a-light>
+    <a-light v-pre type="point" intensity="2" position="0 3 -3" color="#445451"></a-light>
+
+    <a-camera v-pre
         look-controls
+        wasd-controls="acceleration:100"
         position="0 1 4"
         cursor-visible="true"
         cursor-scale="2"
@@ -18,21 +19,34 @@
 </template>
 
 <script>
-
+import {select} from 'd3-selection';
+import {range} from 'd3-array';
 export default {
-  name: 'App',
-  components: {
+  mounted() {
+    const data = range(20).map((i) => ({value: (i + 1) / 10, label: `Item ${i + 1}`}))
+
+    const entity = select('a-scene')
+      .selectAll('a-entity')
+      .data(data)
+      .enter()
+      .append('a-entity')
+      .attr('rotation', (datum, index) => {
+        return `0 ${(60 + index * -7)} 0`;
+      });
+
+    entity.append('a-box')
+      .attr('material', 'color:#08c;')
+      .attr('width', 0.4)
+      .attr('height', ({value}) => value)
+      .attr('position', ({value}) => {
+        return `0 ${value / 2} -5`
+      });
+
+    entity.append('a-entity')
+      .attr('text', ({label}) => `value: ${label};color: black;align:center;`)
+      .attr('position', () => {
+        return `0 .1 -4`
+      });
   }
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
